7317efa04cca9f0e64ba4468ac74dc7d319cc0f4
carloradice <c.radice@campus.unimib.it>
2022-01-27 17:50:11 +0100

    works

--- python/project_velodyne_into_camera.py
+++ python/project_velodyne_into_camera.py
@@ -4,13 +4,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import glob
 import open3d as o3d
+import argparse
 
-from build_pointcloud import build_pointcloud
 from transform import build_se3_transform
 from image import load_image
 from camera_model import CameraModel
-from interpolate_poses import interpolate_vo_poses, interpolate_ins_poses
-from velodyne import load_velodyne_raw, load_velodyne_binary, velodyne_raw_to_pointcloud
+from interpolate_poses import interpolate_vo_poses
+from velodyne import load_velodyne_raw, velodyne_raw_to_pointcloud
 
 
 EXTRINSICS = '/home/carlo/Documents/tesi/server/robotcar-dataset-sdk/extrinsics'
@@ -20,8 +20,8 @@ POSES_FILE = '/home/carlo/Downloads/2019-01-10-14-36-48-radar-oxford-10k-partial
 STEREO_LEFT = '/home/carlo/Downloads/2019-01-10-14-36-48-radar-oxford-10k-partial/stereo/left'
 VELO_L = '/home/carlo/Downloads/2019-01-10-14-36-48-radar-oxford-10k-partial/velodyne_left'
 VELO_R = '/home/carlo/Downloads/2019-01-10-14-36-48-radar-oxford-10k-partial/velodyne_right'
-# IMAGE = '1547131048253167'
 IMAGE = '1547131046378130'
+
 
 def bpc(lidar_dir, poses_file, extrinsics_dir, start_time, end_time, origin_time=-1, interval=None):
     """Builds a pointcloud by combining multiple LIDAR scans with odometry information.
@@ -63,7 +63,6 @@ def bpc(lidar_dir, poses_file, extrinsics_dir, start_time, end_time, origin_time
         extrinsics = next(extrinsics_file)
     G_posesource_laser = build_se3_transform([float(x) for x in extrinsics.split(' ')])
 
-    poses_type = re.search('(vo)\.csv', poses_file).group(1)
 
     # sensor is VO, which is located at the main vehicle frame
     poses = interpolate_vo_poses(poses_file, timestamps, origin_time)
@@ -85,32 +84,14 @@ def bpc(lidar_dir, poses_file, extrinsics_dir, start_time, end_time, origin_time
         tmp = []
         for j in range(0, scan.shape[1]):
             d = np.sqrt(np.power(scan[0, j], 2) + np.power(scan[1, j], 2) + np.power(scan[2, j], 2))
+            # Elimina i punti fino a 2.7 metri di distanza e con il valore della x positivo (dietro la macchina)
             if d > 2.7 and scan[0, j] < 0:
                 tmp.append(scan[:,j])
         tmp = np.array(tmp)
         tmp = np.transpose(tmp)
         scan = tmp
 
-
         scan = np.dot(np.dot(poses[i], G_posesource_laser), np.vstack([scan, np.ones((1, scan.shape[1]))]))
-
-        # tmp = []
-        # for j in range(0, scan.shape[1]):
-        #     d = np.sqrt(np.power(scan[0, j], 2) + np.power(scan[1, j], 2) + np.power(scan[2, j], 2))
-        #     if d > 1:
-        #         tmp.append(scan[:,j])
-        #
-        # #print(len(tmp))
-        # tmp = np.array(tmp)
-        # #print(tmp.shape)
-        # tmp = tmp[:,:,0]
-        # #print(tmp.shape)
-        # tmp = np.transpose(tmp)
-        # #print(tmp.shape)
-        #
-        # scan = tmp
-        #
-        # #print(scan.shape)
 
         if interval == None:
             pointcloud = np.hstack([pointcloud, scan])
@@ -127,6 +108,8 @@ def bpc(lidar_dir, poses_file, extrinsics_dir, start_time, end_time, origin_time
 
 
 def single_time():
+    timestamp = int(IMAGE)
+
     model = CameraModel(MODELS_DIR, STEREO_LEFT)
 
     extrinsics_path = os.path.join(EXTRINSICS, model.camera + '.txt')
@@ -136,16 +119,8 @@ def single_time():
     G_camera_vehicle = build_se3_transform(extrinsics)
     G_camera_posesource = None
 
-    poses_type = re.search('(vo|ins|rtk)\.csv', POSES_FILE).group(1)
-    if poses_type in ['ins', 'rtk']:
-        with open(os.path.join(EXTRINSICS, 'ins.txt')) as extrinsics_file:
-            extrinsics = next(extrinsics_file)
-            G_camera_posesource = G_camera_vehicle * build_se3_transform([float(x) for x in extrinsics.split(' ')])
-    else:
-        # VO frame and vehicle frame are the same
-        G_camera_posesource = G_camera_vehicle
-
-    timestamp = int(IMAGE)
+    # VO frame and vehicle frame are the same
+    G_camera_posesource = G_camera_vehicle
 
     l = []
     for f in sorted(glob.glob(VELO_L+'/*.png')):
@@ -156,7 +131,7 @@ def single_time():
         if v > timestamp and v < n:
             n = v
     print('Next {}'.format(n))
-    pointcloud, reflectance = build_pointcloud(lidar_dir=VELO_L, extrinsics_dir=EXTRINSICS, poses_file=POSES_FILE,
+    pointcloud, reflectance = bpc(lidar_dir=VELO_L, extrinsics_dir=EXTRINSICS, poses_file=POSES_FILE,
                                                start_time=n, end_time=n, origin_time=-1)
     pointcloud_l = np.dot(G_camera_posesource, pointcloud)
     print(pointcloud_l.shape)
@@ -170,7 +145,7 @@ def single_time():
         if v > timestamp and v < n:
             n = v
     print('Next {}'.format(n))
-    pointcloud, reflectance = build_pointcloud(lidar_dir=VELO_R, extrinsics_dir=EXTRINSICS, poses_file=POSES_FILE,
+    pointcloud, reflectance = bpc(lidar_dir=VELO_R, extrinsics_dir=EXTRINSICS, poses_file=POSES_FILE,
                                                start_time=n, end_time=n, origin_time=-1)
 
     pointcloud_r = np.dot(G_camera_posesource, pointcloud)
@@ -191,6 +166,13 @@ def single_time():
     plt.xticks([])
     plt.yticks([])
     plt.show()
+
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(pointcloud[:3].transpose().astype(np.float64))
+    pcd.colors = o3d.utility.Vector3dVector(
+        np.tile(pointcloud[3:].transpose(), (1, 3)).astype(np.float64) / 40)
+
+    o3d.visualization.draw_geometries([pcd])
 
 
 def multi_time():
@@ -259,7 +241,6 @@ def multi_time():
     plt.yticks([])
     plt.show()
 
-
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(pointcloud[:3].transpose().astype(np.float64))
     pcd.colors = o3d.utility.Vector3dVector(
@@ -267,12 +248,15 @@ def multi_time():
 
     o3d.visualization.draw_geometries([pcd])
 
-def main():
-    # Tutto ok
-    # single_time()
 
-    # Da sistemare
-    multi_time()
+def main():
+    parser = argparse.ArgumentParser(description='Project LIDAR data into camera image')
+    parser.add_argument('--mode', type=str, default='single')
+    args = parser.parse_args()
+    if args.mode == 'single':
+        single_time()
+    else:
+        multi_time()
 
 
 if __name__ == '__main__':
